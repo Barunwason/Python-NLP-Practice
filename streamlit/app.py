f7b79be7e43b1ba04f99683dc5c3dc35ff8750dc
Barunwason <barunwason@gmail.com>
2024-08-28 22:26:59 +0530

    commit changes

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("hello streamlit")
 st.write("this is streamlitgit add")
+
+
+df = pd.DataFrame({
+    'first col': [1, 2, 3, 4],
+    'second col': [10, 20, 30, 40]
+})
+
+st.write("this is a dataframe")
+st.write(df)
